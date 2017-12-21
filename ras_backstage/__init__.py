726a487912d5aecd0f5a031d5e030682c965407f
Andrew Millar <andrew.millar@qa.com>
2017-12-21 13:26:00 +0000

    Add tests for party/party-details endpoint

--- ras_backstage/__init__.py
+++ ras_backstage/__init__.py
@@ -33,7 +33,7 @@ api.add_namespace(survey_api)
 import ras_backstage.error_handlers  # NOQA # pylint: disable=wrong-import-position
 from ras_backstage.resources.info import Info  # NOQA # pylint: disable=wrong-import-position
 from ras_backstage.resources.sign_in.sign_in import SignIn  # NOQA # pylint: disable=wrong-import-position
-from ras_backstage.resources.party.create_message import PartyDetails  # NOQA # pylint: disable=wrong-import-position
+from ras_backstage.resources.party.get_party_details import PartyDetails  # NOQA # pylint: disable=wrong-import-position
 from ras_backstage.resources.secure_messaging.get_message_list import GetMessagesList  # NOQA # pylint: disable=wrong-import-position
 from ras_backstage.resources.secure_messaging.get_message import GetMessage  # NOQA # pylint: disable=wrong-import-position
 from ras_backstage.resources.secure_messaging.update_label import RemoveUnreadLabel  # NOQA # pylint: disable=wrong-import-position
