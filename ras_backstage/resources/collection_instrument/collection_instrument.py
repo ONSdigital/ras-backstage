6cfd32964431e6ece442543a4ad11b53c5444b02
Jamie <me@jcox.tech>
2018-01-23 12:20:37 +0000

    US009 Upload sample to rm-sample-service (#29)
    Adds a /backstage-api/v1/sample/<short_name>/<period> POST endpoint for uploading sample files to the sample service and creating a link to the collection exercise when successful for retrieval later.
    
    * Add sample resource and endpoint for uploading samples to the RM service
    * Pass back the response JSON from sample service for frontend to consume
    * After uploading a sample file make sure an exercise is linked to it

--- ras_backstage/resources/collection_instrument/collection_instrument.py
+++ ras_backstage/resources/collection_instrument/collection_instrument.py
@@ -1,7 +1,7 @@
 import logging
 
 from flask import make_response, jsonify, request, Response
-from flask_restplus import Resource
+from flask_restplus import Resource, reqparse
 from structlog import wrap_logger
 
 from ras_backstage import collection_instrument_api
@@ -9,13 +9,18 @@ from ras_backstage.common.filters import get_collection_exercise_by_period
 from ras_backstage.controllers import survey_controller, collection_exercise_controller
 from ras_backstage.controllers.collection_instrument_controller import upload_collection_instrument
 
+
 logger = wrap_logger(logging.getLogger(__name__))
+
+parser = reqparse.RequestParser()
+parser.add_argument('file', location='files', required=True)
 
 
 @collection_instrument_api.route('/<short_name>/<period>')
 class CollectionInstrument(Resource):
 
     @staticmethod
+    @collection_instrument_api.expect(parser)
     def post(short_name, period):
         logger.info('Uploading collection instrument', shortname=short_name, period=period)
         survey = survey_controller.get_survey_by_shortname(short_name)
@@ -26,5 +31,6 @@ class CollectionInstrument(Resource):
             return make_response(jsonify({"message": "Collection exercise not found"}), 404)
 
         upload_collection_instrument(survey['id'], exercise['id'], request.files['file'])
+
         logger.info('Successfully retrieved collection exercise details', shortname=short_name, period=period)
         return Response(status=201)
