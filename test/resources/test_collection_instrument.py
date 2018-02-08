00d247aa762448d3540eae6f61fead1514995f69
Andrew Millar <andrew.millar@qa.com>
2018-02-08 17:49:03 +0000

    Remove unnecessary f-string

--- test/resources/test_collection_instrument.py
+++ test/resources/test_collection_instrument.py
@@ -41,7 +41,7 @@ class TestCollectionExercise(unittest.TestCase):
     def test_upload_collection_instrument(self, mock_request):
         mock_request.get(url_get_survey_by_short_name, json=self.survey)
         mock_request.get(url_ces, json=self.collection_exercises)
-        mock_request.post(f'{url_upload_collection_instrument}', additional_matcher=self.file_matcher)
+        mock_request.post(url_upload_collection_instrument, additional_matcher=self.file_matcher)
 
         response = self.app.post('/backstage-api/v1/collection-instrument/test/000000', data=dict(
             file=(BytesIO(b'data'), 'test.xlsx')))
