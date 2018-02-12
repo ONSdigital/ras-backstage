1c99814825d1f9eda1c2a3ef36aca7013cc5a2c2
James Gardiner <james@jgardiner.co.uk>
2018-02-12 14:03:18 +0000

    Return 200 instead of 201 when an oauth token is retrieved

--- ras_backstage/resources/sign_in/sign_in.py
+++ ras_backstage/resources/sign_in/sign_in.py
@@ -9,18 +9,17 @@ from structlog import wrap_logger
 from ras_backstage import app, sign_in_api, sign_in_api_v2
 from ras_backstage.controllers import django_controller, uaa_controller
 
-
 logger = wrap_logger(logging.getLogger(__name__))
 
-sign_in_details = sign_in_api.model('SignInDetails', {
-    'username': fields.String(required=True, description='username'),
-    'password': fields.String(required=True, description='password')
-})
+sign_in_details = sign_in_api.model(
+    'SignInDetails', {
+        'username': fields.String(required=True, description='username'),
+        'password': fields.String(required=True, description='password')
+    })
 
 
 @sign_in_api.route('/', methods=['POST'])
 class SignIn(Resource):
-
     @staticmethod
     @sign_in_api.expect(sign_in_details, validate=True)
     def post():
@@ -30,13 +29,15 @@ class SignIn(Resource):
         password = message_json['password']
 
         oauth2_token = django_controller.sign_in(username, password)
-        token_expiry = datetime.now() + timedelta(seconds=int(oauth2_token['expires_in']))
+        token_expiry = datetime.now() + timedelta(
+            seconds=int(oauth2_token['expires_in']))
         oauth2_token['expires_at'] = token_expiry.timestamp()
         oauth2_token['party_id'] = 'BRES'
         oauth2_token['role'] = 'internal'
 
         jwt_secret = app.config['JWT_SECRET']
-        token = jwt.encode(oauth2_token, jwt_secret, algorithm=app.config['JWT_ALGORITHM'])
+        token = jwt.encode(
+            oauth2_token, jwt_secret, algorithm=app.config['JWT_ALGORITHM'])
 
         logger.info('Successfully retrieved sign-in details')
         return make_response(jsonify({"token": token}), 201)
@@ -44,7 +45,6 @@ class SignIn(Resource):
 
 @sign_in_api_v2.route('/', methods=['POST'])
 class SignInV2(Resource):
-
     @staticmethod
     @sign_in_api_v2.expect(sign_in_details, validate=True)
     def post():
@@ -57,12 +57,15 @@ class SignInV2(Resource):
             logger.info('Retrieving sign-in details')
             oauth2_token = uaa_controller.sign_in(username, password)
             logger.info('Successfully retrieved sign-in details')
-            return make_response(jsonify({"token": oauth2_token}), 201)
+            return make_response(jsonify({"token": oauth2_token}), 200)
         else:
             #  TODO remove this once UAA fully deployed in all environments
             if username == current_app.config['USERNAME'] and password == current_app.config['PASSWORD']:
                 logger.info("Authentication successful", user=username)
-                return make_response(jsonify({"token": "1234abc"}), 201)
+                return make_response(jsonify({"token": "1234abc"}), 200)
             else:
                 logger.info("Authentication failed", user=username)
-                return make_response(jsonify({"error": "Username and/or password incorrect"}), 401)
+                return make_response(
+                    jsonify({
+                        "error": "Username and/or password incorrect"
+                    }), 401)
