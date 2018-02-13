f0747339ecf59e793cb3bf5df91e016f0d951996
Andrew Millar <andrew.millar@qa.com>
2018-02-13 15:23:37 +0000

    Clean up reporting unit page

--- ras_backstage/resources/reporting_units/get_reporting_unit.py
+++ ras_backstage/resources/reporting_units/get_reporting_unit.py
@@ -18,29 +18,19 @@ class GetReportingUnit(Resource):
     @staticmethod
     def get(ru_ref):
         logger.info('Retrieving reporting unit details', ru_ref=ru_ref)
-        # Retrieve reporting unit
+
         reporting_unit = party_controller.get_party_by_ru_ref(ru_ref)
 
-        # Retrieve surveys for the reporting unit
-        survey_ids = []
-        for respondent in reporting_unit.get('associations'):
-            for enrolment in respondent.get('enrolments'):
-                survey_ids.append(enrolment.get('surveyId'))
-        unique_survey_ids = set(survey_ids)
-        surveys = [survey_controller.get_survey_by_id(survey_id) for survey_id in unique_survey_ids]
+        surveys = get_surveys_for_reporting_unit(reporting_unit)
 
-        # Retrieve cases for the reporting unit
         cases = case_controller.get_cases_by_business_party_id(reporting_unit['id'])
         case_collection_exercise_ids = [case['caseGroup']['collectionExerciseId'] for case in cases]
 
         for survey in surveys:
-            survey_collection_exercises = []
             ces = collection_exercise_controller.get_collection_exercises_by_survey(survey['id'])
-            for ce in ces:
-                survey_collection_exercises.append(ce)
-            survey['collection_exercises'] = [ce for ce in survey_collection_exercises if ce['id'] in case_collection_exercise_ids]
+            survey['collection_exercises'] = [ce for ce in ces if ce['id'] in case_collection_exercise_ids]
 
-        for survey in surveys:
+            # Add collection exercise details
             for exercise in survey['collection_exercises']:
                 exercise['responseStatus'] = get_case_group_status_by_collection_exercise(cases, exercise['id'])
                 reporting_unit_ce = party_controller.get_party_by_business_id(reporting_unit['id'], exercise['id'])
@@ -53,3 +43,12 @@ class GetReportingUnit(Resource):
         }
         logger.info('Successfully retrieved reporting unit details', ru_ref=ru_ref)
         return make_response(jsonify(response_json), 200)
+
+
+def get_surveys_for_reporting_unit(reporting_unit):
+    survey_ids = []
+    for respondent in reporting_unit.get('associations'):
+        for enrolment in respondent.get('enrolments'):
+            survey_ids.append(enrolment.get('surveyId'))
+    unique_survey_ids = set(survey_ids)
+    return [survey_controller.get_survey_by_id(survey_id) for survey_id in unique_survey_ids]
