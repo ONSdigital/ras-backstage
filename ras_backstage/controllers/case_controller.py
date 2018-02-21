5d51b34be88b9b482772dd586cf0a44f4a4a59ee
Tejas Patel <tejas.patel@ons.gov.uk>
2018-02-21 11:38:14 +0000

    catch both not found scenarios

--- ras_backstage/controllers/case_controller.py
+++ ras_backstage/controllers/case_controller.py
@@ -15,7 +15,7 @@ def get_cases_by_business_party_id(business_party_id):
     url = f'{app.config["RM_CASE_SERVICE"]}cases/partyid/{business_party_id}'
     response = request_handler('GET', url, auth=app.config['BASIC_AUTH'], params={"iac": "True"})
 
-    if response.status_code == 404:
+    if response.status_code == 404 or response.status_code == 204:
         logger.debug('No cases found for business', business_party_id=business_party_id)
         return []
     if response.status_code != 200:
