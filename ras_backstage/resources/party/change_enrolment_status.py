b97842f9f904c29d26f0e43c38d60a9c6e98c49d
Gemma Irving <gemma.irving@ons.gov.uk>
2018-03-09 16:01:22 +0000

    Original implementation for changing enrolment status

---
+++ ras_backstage/resources/party/change_enrolment_status.py
@@ -0,0 +1,33 @@
+import logging
+
+from flask import jsonify, make_response, request
+from flask_restplus import fields, Resource
+from structlog import wrap_logger
+
+from ras_backstage import party_api
+from ras_backstage.controllers import party_controller
+
+logger = wrap_logger(logging.getLogger(__name__))
+
+enrolment_details = party_api.model('EnrolmentDetails', {
+    'respondent_party_id': fields.String(required=True),
+    'business_party_id': fields.String(required=True),
+    'survey_id': fields.String(required=True),
+    'change_flag': fields.String(required=True)
+}
+)
+
+
+@party_api.route('/change-enrolment-status')
+class ChangeEnrolmentStatus(Resource):
+
+    @staticmethod
+    @party_api.expect(enrolment_details, validate=True)
+    def put():
+        enrolment_json = request.get_json()
+        logger.info('Changing respondent enrolment status')
+
+        response = party_controller.put_respondent_enrolment_status(enrolment_json)
+
+        logger.info('Successfully changed enrolment status')
+        return make_response(jsonify(response), 200)
