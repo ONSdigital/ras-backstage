7c55227920b161fcfb8b4485929ea33b397c645e
Mark Price <markprice1973@gmail.com>
2018-03-28 14:15:08 +0100

    Preparing release_18

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 class Config(object):
     DEBUG = False
     TESTING = False
-    VERSION = '0.1.1'
+    VERSION = '0.1.3'
     PORT = os.getenv('PORT', 8001)
     LOGGING_LEVEL = os.getenv('LOGGING_LEVEL', 'INFO')
     SECURITY_USER_NAME = os.getenv('SECURITY_USER_NAME')
