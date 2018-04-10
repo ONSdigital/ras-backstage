249ded6678a9879007115b7ef370c6bdf0383a22
Richard Weeks <richard.weeks@ons.gov.uk>
2018-04-10 11:50:17 +0100

    Add Reporting controller following review comments.

--- ras_backstage/controllers/collection_exercise_controller.py
+++ ras_backstage/controllers/collection_exercise_controller.py
@@ -10,18 +10,6 @@ from ras_backstage.exception.exceptions import ApiError
 logger = wrap_logger(logging.getLogger(__name__))
 
 
-def download_collection_exercise_report(collection_exercise_id):
-    logger.debug('Downloading Collection exercise report', collection_exerise_id=collection_exercise_id)
-    url = f'{app.config["RM_REPORT_SERVICE"]}reporting-api/v1/response-chasing/download-report/{collection_exercise_id}'
-    response = request_handler('GET', url, auth=app.config['BASIC_AUTH'])
-    if response.status_code != 200:
-        logger.error('Error retrieving collection exercise', collection_exercise_id=collection_exercise_id)
-        raise ApiError(url, response.status_code)
-
-    logger.debug('Successfully retrieved collection exercise', collection_exercise_id=collection_exercise_id)
-    return response.content, response.headers.items()
-
-
 def get_collection_exercise_by_id(collection_exercise_id):
     logger.debug('Retrieving collection exercise', collection_exercise_id=collection_exercise_id)
     url = f'{app.config["RM_COLLECTION_EXERCISE_SERVICE"]}collectionexercises/{collection_exercise_id}'
